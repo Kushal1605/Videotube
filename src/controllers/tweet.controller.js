65d31e1ec858e4aefe9fed67a4a307f487643cd3
Kushal1605 <kushalgupta1605@gmail.com>
2024-08-01 14:47:38 +0530

    tweet controller

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,126 @@
+import { isValidObjectId } from "mongoose";
+import { Tweet } from "../models/tweet.model.js";
+import ApiError from "../utils/ApiError.js";
+import ApiResponse from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+const createTweet = asyncHandler(async (req, res) => {
+  const { content } = req.body;
+
+  if (!content?.trim()) {
+    throw new ApiError(400, "Cannot create empty tweet");
+  }
+
+  const tweet = await Tweet.create({
+    owner: req.user._id,
+    content,
+  });
+
+  if (!tweet) {
+    throw new ApiError(500, "Something went wrong creating tweet");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet created successfully."));
+});
+
+const updateTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+  const { content } = req.body;
+
+  if (!tweetId?.trim() || !isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet id");
+  }
+
+  if (!content?.trim()) {
+    throw new ApiError(400, "Cannot update with empty tweet");
+  }
+
+  const updatedTweet = await Tweet.findByIdAndUpdate(
+    tweetId,
+    { $set: { content } },
+    { new: true }
+  );
+
+  if (!updatedTweet) {
+    throw new ApiError(500, "Unable to update the tweet.");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, updateTweet, "Tweet updated successfully"));
+});
+
+const deleteTweet = asyncHandler(async (req, res) => {
+  const { tweetId } = req.params;
+
+  if (!tweetId?.trim() || !isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet id.");
+  }
+
+  const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
+
+  if (!deletedTweet) {
+    throw new ApiError(500, "Something went wrong while deleting the tweet");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, deleteTweet, "Tweet deleted successfully"));
+});
+
+const getUserTweets = asyncHandler(async (req, res) => {
+  const tweets = Tweet.aggregate([
+    {
+      $match: {
+        owner: req.user._id,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweet",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+        likesCount: {
+          $size: "$likes",
+        },
+      },
+    },
+  ]);
+
+  if (!tweets) {
+    throw new ApiError(500, "Something went wrong while fetching tweets");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(201, tweets, "Tweets fetched successfully."));
+});
+
+export { createTweet, updateTweet, deleteTweet, getUserTweets };
